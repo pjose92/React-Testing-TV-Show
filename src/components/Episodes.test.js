cc5623b2f2f74afed733517091e743102ca75d14
Jose <pjose14.jp38@yahoo.com>
2020-04-24 17:46:21 -0500

    MVP all good to go

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Episodes from './Episodes';
+
+test('rendering without any episodes at all', () => {
+	render(<Episodes episodes={[]} />);
+});
+
+test('it renders list of the episodes', () => {
+	const mockEpisode = [
+		{ id: 'id909342', name: 'Chapter Two: Trick or Treat, Freak' },
+	];
+	const { queryAllByText, rerender } = render(<Episodes episodes={[]} />);
+
+	let allEpisodes = queryAllByText(/season/i);
+	expect(allEpisodes).toHaveLength(0);
+
+	rerender(<Episodes episodes={mockEpisode} />);
+	allEpisodes = queryAllByText(/Season/i);
+	expect(allEpisodes).toHaveLength(1);
+});
